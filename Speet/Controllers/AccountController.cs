a85a4aef59ae3d1618f09dc395a004c82d21cae3
MarioSchenkewitz <90987509+MarioSchenkewitz@users.noreply.github.com>
2021-10-19 13:49:26 +0200

    Added Cookie to GoogleResponse() in AccountController

--- Speet/Controllers/AccountController.cs
+++ Speet/Controllers/AccountController.cs
@@ -2,10 +2,12 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Speet.Controllers
@@ -16,7 +18,7 @@ namespace Speet.Controllers
         [Route("google-login")]
         public IActionResult GoogleLogin()
         {
-            var properties = new AuthenticationProperties { RedirectUri = "/" };
+            var properties = new AuthenticationProperties { RedirectUri = Url.Action("GoogleResponse") };
             return Challenge(properties, GoogleDefaults.AuthenticationScheme);
 
             
@@ -25,9 +27,13 @@ namespace Speet.Controllers
         [Route("Index")]
         public async Task<IActionResult> GoogleResponse()
         {
+            /*
             var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            var claims = result.Principal.Claims.ToList();
+            var UserId = claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+            */
 
-
+            /*
             var claims = result.Principal.Identities
                 .FirstOrDefault().Claims.Select(claim => new
                 {
@@ -36,9 +42,23 @@ namespace Speet.Controllers
                     claim.Type,
                     claim.Value
                 });
+            */
 
-            return Json(claims);
+            //get NameIdentifier
+            var userId = HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            // Cookie here
+            Response.Cookies.Append(userId, "12345", new CookieOptions()
+            {
+                Expires = DateTimeOffset.Now.AddHours(4),
+                Path = "/",
+                HttpOnly = true,
+                Secure = true,
+            });
+
+            return RedirectToAction("DiscoverGroups", "SportGroup");
         }
+         
 
         [HttpGet, Route("Logout")]
         public async Task<IActionResult> Logout()
